efb5b22928756692c23be403d3325425a3539d31
Miles Urquidi <milesurquidi@yahoo.com>
2026-04-10 16:07:55 -0700

    vercel tracking put in

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { Analytics } from "@vercel/analytics/next";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 
@@ -36,6 +37,7 @@ export default function RootLayout({
       <body className="min-h-full flex flex-col">
         <Navbar />
         {children}
+        <Analytics />
       </body>
     </html>
   );
